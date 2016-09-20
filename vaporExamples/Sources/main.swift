92b664523c6a5564a35c5be4818814ffa639ddce
leandrodeleite <leandrodeleite@gmail.com>
2016-09-20 17:38:57 -0300

    Add Vapor examples

--- vaporExamples/Sources/main.swift
+++ vaporExamples/Sources/main.swift
@@ -6,10 +6,18 @@ let drop = Droplet()
 
 drop.get("/hello") { _ in
   return "Hello Vapor"
-}
+} //      /hello
 
 drop.get("hi", String.self) { request, name in
   return "Hi, \(name)."
+} //    /hi/string
+
+drop.get("hey") { req in
+  return "Hey!"
+}
+
+drop.get("users", Int.self) { request, userId in
+  return "You requested User #\(userId)"
 }
 
 drop.run()
