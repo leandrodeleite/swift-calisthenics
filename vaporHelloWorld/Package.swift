2932003e1c7c748c4e0d523551800acf11893495
leandrodeleite <leandrodeleite@gmail.com>
2016-09-19 23:43:38 -0300

    Add Vapor Package dependencies

--- vaporHelloWorld/Package.swift
+++ vaporHelloWorld/Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "vaporHelloWorld"
+    name: "vaporHelloWorld",
+    dependencies: [
+      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+    ]
 )
+
