3d81e5916659bd5c983e155a8f3547ef3edaf8fa
leandrodeleite <leandrodeleite@gmail.com>
2016-09-20 15:19:48 -0300

    Fix Vapor dependency

--- vaporExamples/Package.swift
+++ vaporExamples/Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "vaporExamples",
     dependencies: [
         .Package(
-            url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: y)
+            url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
     ]
 )
