24ae883dad07ce65541ff3304370eda3f365f6bc
leandrodeleite <leandrodeleite@gmail.com>
2016-09-21 23:59:53 -0300

    Add Zewo basic server routing app

--- zewoHelloWorld/Sources/main.swift
+++ zewoHelloWorld/Sources/main.swift
@@ -1,1 +1,12 @@
 print("Hello, world!")
+
+import HTTPServer
+
+let router = BasicRouter { route in
+    route.get("/hello") { request in
+        return Response(body: "Hello, world!")
+    }
+}
+
+let server = try Server(configuration: ["port": 8080], responder: router)
+try server.start()
