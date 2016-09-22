b3c58ae1f26d2158c10fbad46bb2c6ce91a8b682
leandrodeleite <leandrodeleite@gmail.com>
2016-09-21 23:54:12 -0300

    Add Zewo package depencencies

--- zewoHelloWorld/Package.swift
+++ zewoHelloWorld/Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "zewoHelloWorld"
+    name: "zewoHelloWorld",
+    dependencies : [
+      .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 13)
+    ]
 )
