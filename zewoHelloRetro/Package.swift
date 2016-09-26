6631a5800550961a41f449fb3bad57607fd4a265
leandrodeleite <leandrodeleite@gmail.com>
2016-09-26 13:03:28 -0300

    [Zewo] Set dependencies package to recent version

--- zewoHelloRetro/Package.swift
+++ zewoHelloRetro/Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "zewoHelloRetro",
     dependencies: [
-      .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 8),
-      .Package(url: "https://github.com/Zewo/Router.git",     majorVersion: 0, minor: 8),
+      .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 13),
     ]
 )
