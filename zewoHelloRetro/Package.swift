32996d0adc970342b3632a08ae41c32d37dbc1e1
leandrodeleite <leandrodeleite@gmail.com>
2016-09-26 12:58:12 -0300

    [Zewo] Add dependencies packages, error

--- zewoHelloRetro/Package.swift
+++ zewoHelloRetro/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "zewoHelloRetro",
     dependencies: [
-      .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 7),
-      .Package(url: "https://github.com/Zewo/Router.git",     majorVersion: 0, minor: 7),
+      .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 8),
+      .Package(url: "https://github.com/Zewo/Router.git",     majorVersion: 0, minor: 8),
     ]
 )
