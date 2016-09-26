472aa916b5a48dc320ba2b8bc14cb4b64f9c37a3
leandrodeleite <leandrodeleite@gmail.com>
2016-09-26 12:44:17 -0300

    [Zewo] Add Zewo HTTPServer/Router older version

--- zewoHelloRetro/Package.swift
+++ zewoHelloRetro/Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "zewoHelloRetro"
+    name: "zewoHelloRetro",
+    dependencies: [
+      .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 7),
+      .Package(url: "https://github.com/Zewo/Router.git",     majorVersion: 0, minor: 7),
+    ]
 )
