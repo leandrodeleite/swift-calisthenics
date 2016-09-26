b4a42ef5097d3c75a1675acca44bc445ae4a1543
leandrodeleite <leandrodeleite@gmail.com>
2016-09-26 13:10:23 -0300

    [Zewo] Import dependencies, older code, error

--- zewoHelloRetro/Sources/main.swift
+++ zewoHelloRetro/Sources/main.swift
@@ -1,1 +1,15 @@
 print("Hello, world!")
+
+import HTTPServer
+import Router
+
+let app = Router { route in
+  route.get("/hello") { request in
+    return Response(body: "Hello, Zewo!")
+  }
+  route.get("/") { request in
+    return Response(body: "Zewo")
+  }
+}
+
+try Server(app).start()
